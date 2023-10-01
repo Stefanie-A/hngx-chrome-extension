652a26f7303a581ffcac942ccb01829ee7c8f330
Stefanie-A <102748373+Stefanie-A@users.noreply.github.com>
2023-10-01 23:43:05 +0100

    modification

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def download_video(filename):
 
 if __name__ == "__main__":
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
-    app.run(debug=True, port=8000)
+    app.run(debug=True)
